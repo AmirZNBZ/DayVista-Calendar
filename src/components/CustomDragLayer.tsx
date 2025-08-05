1c7f05352714ce95f86a8229a09b0f5a2097484f
Amir Nabizade <amirnabiizade@gmail.com>
2025-08-05 15:48:12 +0330

    fix mouse pointer offset in movement

--- src/components/CustomDragLayer.tsx
+++ src/components/CustomDragLayer.tsx
@@ -1,37 +1,48 @@
 import React from "react";
-import { useDragLayer } from "react-dnd";
+import { useDragLayer, type XYCoord } from "react-dnd";
 import DateObject from "react-date-object";
 
 const layerStyles: React.CSSProperties = {
-  position: "fixed",
-  pointerEvents: "none",
-  zIndex: 100,
-  left: 0,
   top: 0,
+  left: 0,
+  zIndex: 100,
   width: "100%",
   height: "100%",
+  position: "fixed",
+  pointerEvents: "none",
 };
 
 function getItemStyles(
-  initialOffset: { x: number; y: number } | null,
-  currentOffset: { x: number; y: number } | null
+  initialSourceOffset: XYCoord | null,
+  initialPointerOffset: XYCoord | null,
+  currentPointerOffset: XYCoord | null
 ) {
-  if (!initialOffset || !currentOffset) {
+  if (!initialSourceOffset || !initialPointerOffset || !currentPointerOffset) {
     return { display: "none" };
   }
-  const { x, y } = currentOffset;
+
+  // محاسبه میزان جابجایی ماوس از نقطه شروع
+  const deltaX = currentPointerOffset.x - initialPointerOffset.x;
+  const deltaY = currentPointerOffset.y - initialPointerOffset.y;
+
+  // موقعیت نهایی = موقعیت اولیه آیتم + میزان جابجایی ماوس
+  const x = initialSourceOffset.x + deltaX;
+  const y = initialSourceOffset.y + deltaY;
   const transform = `translate(${x}px, ${y}px)`;
+
   return { transform };
 }
 
 export const CustomDragLayer = () => {
-  const { item, itemType, isDragging, initialOffset, currentOffset } = useDragLayer((monitor) => ({
-    item: monitor.getItem(),
-    itemType: monitor.getItemType(),
-    initialOffset: monitor.getInitialSourceClientOffset(),
-    currentOffset: monitor.getClientOffset(),
-    isDragging: monitor.isDragging(),
-  }));
+  const { item, itemType, isDragging, currentPointerOffset, initialPointerOffset, initialSourceOffset } =
+    useDragLayer((monitor) => ({
+      item: monitor.getItem(),
+      itemType: monitor.getItemType(),
+      isDragging: monitor.isDragging(),
+      currentPointerOffset: monitor.getClientOffset(),
+      initialPointerOffset: monitor.getInitialClientOffset(),
+      initialSourceOffset: monitor.getInitialSourceClientOffset(),
+    }));
 
   if (!isDragging || itemType !== "event") {
     return null;
@@ -51,7 +62,7 @@ export const CustomDragLayer = () => {
 
   return (
     <div style={layerStyles}>
-      <div style={getItemStyles(initialOffset, currentOffset)}>
+      <div style={getItemStyles(initialSourceOffset, initialPointerOffset, currentPointerOffset)}>
         <div
           style={{ backgroundColor: item.color, width: previewWidth }}
           className="rounded px-1 text-sm text-white opacity-75"
